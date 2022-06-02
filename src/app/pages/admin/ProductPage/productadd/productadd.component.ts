32c0d83f56e2efa2174eb791666eb94b19563ff7
Hoang Nam <namhoangde2002@gmail.com>
2022-06-02 07:45:27 +0700

    add

--- src/app/pages/admin/ProductPage/productadd/productadd.component.ts
+++ src/app/pages/admin/ProductPage/productadd/productadd.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from 'src/app/components/models/Products';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-productadd',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductaddComponent implements OnInit {
 
-  constructor() { }
+  product: IProduct = {
+    name: "",
+    price: 0,
+    status: true
+  }
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private acctivedRouter: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    // lấy id
+    const id = this.acctivedRouter.snapshot.paramMap.get('id')
+    // nếu có id thì call service get product để lấy thông tin về form
+    this.productService.getProduct(id).subscribe(data => this.product = data)
+  }
+  onSubmit(){
+    const id = this.acctivedRouter.snapshot.paramMap.get('id');
+    if (id) {
+      // call service edit product
+      // nếu thành công thì trả về sản phẩm vừa cập nhật xong 
+      this.productService.updateProduct(this.product).subscribe(data => console.log(data))
+    }else{
+      // call service add product
+      this.productService.addProduct(this.product).subscribe(data => {
+        // chuyển hướng router
+        this.router.navigateByUrl('/product');
+      })
+    }
   }
 
 }
